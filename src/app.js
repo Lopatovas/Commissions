b6a3103caa39a214157621fd2d8d3af616eff21e
Lopatovas <lopatovas@gmail.com>
2019-09-19 21:34:43 +0300

    Final code commit

--- src/app.js
+++ src/app.js
@@ -1,44 +1,28 @@
 import { cashInCommision, cashOutCommision } from './Logic/calculator';
+import OPERATION_TYPE from './Constants/OperationType';
 
-const users = [];
+const fs = require('fs');
 
-const data = [
-  {
-    date: '2016-01-05', user_id: 1, user_type: 'natural', type: 'cash_in', operation: { amount: 200.00, currency: 'EUR' },
-  },
-  {
-    date: '2016-01-06', user_id: 2, user_type: 'juridical', type: 'cash_out', operation: { amount: 300.00, currency: 'EUR' },
-  },
-  {
-    date: '2016-01-06', user_id: 1, user_type: 'natural', type: 'cash_out', operation: { amount: 30000, currency: 'EUR' },
-  },
-  {
-    date: '2016-01-07', user_id: 1, user_type: 'natural', type: 'cash_out', operation: { amount: 1000.00, currency: 'EUR' },
-  },
-  {
-    date: '2016-01-07', user_id: 1, user_type: 'natural', type: 'cash_out', operation: { amount: 100.00, currency: 'EUR' },
-  },
-  {
-    date: '2016-01-10', user_id: 1, user_type: 'natural', type: 'cash_out', operation: { amount: 100.00, currency: 'EUR' },
-  },
-  {
-    date: '2016-01-10', user_id: 2, user_type: 'juridical', type: 'cash_in', operation: { amount: 1000000.00, currency: 'EUR' },
-  },
-  {
-    date: '2016-01-10', user_id: 3, user_type: 'natural', type: 'cash_out', operation: { amount: 1000.00, currency: 'EUR' },
-  },
-  {
-    date: '2016-02-15', user_id: 1, user_type: 'natural', type: 'cash_out', operation: { amount: 300.00, currency: 'EUR' },
-  },
-];
+const args = process.argv.slice(2);
 
-for (let i = 0; i < data.length; i += 1) {
-  switch (data[i].type) {
-    case 'cash_in':
-      console.log(cashInCommision(data[i].operation));
-      break;
-    default:
-      console.log(cashOutCommision(data[i], users));
-      break;
+const fileContents = fs.readFileSync(args[0], 'utf8');
+
+try {
+  const data = JSON.parse(fileContents);
+  const users = [];
+  for (let i = 0; i < data.length; i += 1) {
+    switch (data[i].type) {
+      case OPERATION_TYPE.CASH_IN:
+        console.log(cashInCommision(data[i].operation));
+        break;
+      case OPERATION_TYPE.CASH_OUT:
+        console.log(cashOutCommision(data[i], users));
+        break;
+      default:
+        console.log(`${data[i].type} is not a valid operation`);
+        break;
+    }
   }
+} catch (err) {
+  console.error(err);
 }
